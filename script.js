0daabeb3021fcdb58e5ad68e94a468eab88eef7c
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-09-17 12:49:24 +0100

    pricing

---
+++ script.js
@@ -0,0 +1,215 @@
+function showlist1() {
+  document.getElementById("galleryright").innerHTML = `
+
+    <table>
+            <tr>
+              <th>Inches</th>
+              <th>Bone Straight</th>
+              <th>Deep curls</th>
+              <th>Body wave</th>
+              <th>Kinky curls</th>
+            </tr>
+            <tr>
+              <td>8"</td>
+              <td>€250.00</td>
+              <td>€290.00</td>
+            </tr>
+            <tr>
+              <td>10"</td>
+              <td>€280.00</td>
+              <td>€300.00</td>
+            </tr>
+            <tr>
+              <td>12"</td>
+              <td>€300.00</td>
+              <td>€330.00</td>
+            </tr>
+            <tr>
+              <td>14"</td>
+              <td>€330.00</td>
+              <td>€350.00</td>
+            </tr>
+            <tr>
+              <td>16"</td>
+              <td>€350.00</td>
+              <td>€370.00</td>
+            </tr>
+            <tr>
+              <td>18"</td>
+              <td>€370.00</td>
+              <td>€390.00</td>
+            </tr>
+            <tr>
+              <td>20"</td>
+              <td>€390.00</td>
+              <td>€400.00</td>
+            </tr>
+            <tr>
+              <td>22"</td>
+              <td>€420.00</td>
+              <td>€450.00</td>
+            </tr>
+            <tr>
+              <td>24"</td>
+              <td>€470.00</td>
+              <td>€490.00</td>
+            </tr>
+            <tr>
+              <td>26"</td>
+              <td>€530.00</td>
+              <td>€550.00</td>
+            </tr>
+            <tr>
+              <td>28"</td>
+              <td>€590.00</td>
+              <td>€600.00</td>
+            </tr>
+            <tr>
+              <td>30"</td>
+              <td>€650.00</td>
+              <td>€690.00</td>
+            </tr>
+            <tr>
+              <td>32"</td>
+              <td>€700.00</td>
+              <td>€750.00</td>
+            </tr>
+            <tr>
+              <td>34"</td>
+              <td>€770.00</td>
+              <td>€790.00</td>
+            </tr>
+            <tr>
+              <td>36"</td>
+              <td>€800.00</td>
+              <td>€850.00</td>
+            </tr>
+              
+          </table>
+          `;
+}
+
+function showlist2() {
+  document.getElementById("galleryright").innerHTML = `
+
+    <table>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+          `;
+}
+function showlist3() {
+  document.getElementById("galleryright").innerHTML = `
+    <table>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+          `;
+}
+function showlist4() {
+  document.getElementById("galleryright").innerHTML = `
+    <table>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+          `;
+}
